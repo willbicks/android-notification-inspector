a13d15d4160095b62fefc2ed040b8324fc1ff8ee
Will Bicks <will@willbicks.com>
2026-03-22 20:17:11 -0500

    Update default theme for DayNight / Compose compatibility

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,7 +39,7 @@ android {
 dependencies {
   // Core Android
   implementation("androidx.core:core-ktx:1.12.0")
-  implementation("androidx.appcompat:appcompat:1.6.1")
+  implementation("com.google.android.material:material:1.13.0")
   implementation("androidx.activity:activity-compose:1.8.2")
 
   // Compose BOM - manages versions for all Compose libraries
